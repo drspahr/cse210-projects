f37a6ae6ad6811dc905e12e5b55bf35c78ef17bd
drspahr <spahrdg@gmail.com>
2023-01-07 11:21:02 -0600

    W01 Parts 1-2 Complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string f_nam = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string l_nam = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {l_nam}, {f_nam} {l_nam}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeFromUser = Console.ReadLine();
+
+        float gradePercent = float.Parse(gradeFromUser);
+        string letter;
+
+        if (gradePercent >= 90)
+        {
+            /*Console.WriteLine("Your letter grade is: A");*/    // Core 1
+            letter = "A";                                        // Core 3
+        }
+        else if (gradePercent < 90 && gradePercent >= 80)
+        {
+            /*Console.WriteLine("Your letter grade is: B");*/
+            letter = "B";
+        }
+        else if (gradePercent < 80 && gradePercent >= 70)
+        {
+            /*Console.WriteLine("Your letter grade is: C");*/
+            letter = "C";
+        }
+        else if (gradePercent < 70 && gradePercent >= 60)
+        {
+            /*Console.WriteLine("Your letter grade is: D");*/
+            letter = "D";
+        }
+        else
+        {
+            /*Console.WriteLine("Your letter grade is: F");*/
+            letter = "F";
+        }
+
+        float remainder = gradePercent % 10;                    // Stretch 1
+        string sign;
+
+        if (remainder > 7 && (letter != "A" && letter != "F"))                                                   // Stretch 1, Stretch 3 and Stretch 3
+        {
+            sign = "+";
+        }
+        else if (remainder < 3 && letter != "F")                // Stretch 3
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = " ";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");     // Core 3 and Stretch 1
+
+        if (gradePercent >= 70)                                         // Core 2
+        {
+            Console.WriteLine("Congratulations! You have passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. If you need help there is help available and continue to work hard. You can do it!");
+        }
     }
 }
